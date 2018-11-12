065ecf2a3e765b3c3f7d6a995514ab8cd7e57037
Momin Khan <foolmoron@gmail.com>
2018-11-12 13:59:24 -0500

    example before_generate fetching

---
+++ themes/rvagj/scripts/beforeRender.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+hexo.extend.filter.register('before_generate', function() {
+	console.log('fetching something before generate...');
+	return axios.get('http://slowwly.robertomurray.co.uk/delay/1000/url/http://textfiles.com/uploads/bannerhack.txt')
+		.then(res => {
+			console.log(res.data);
+		})
+		;
+});
